10f61178dfc3553ba6194d8ec7c116220b30dc1a
Catsy <lockdead456@gmail.com>
2019-05-08 22:13:08 -0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+#fibonacci + matemática simples
+x = 0
+a = 1 + x
+b = a + 1
+c = b + 1
+d = c + b
+e = d + c
+f = d + e
+g = e + f
+h = f + g
+i = g + h
+print('1')
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
